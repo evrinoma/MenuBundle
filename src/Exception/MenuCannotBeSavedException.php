8b3f918a812a77cd25065c6f53ffcb8a25cb973f
Nikolay Nikolaev <evrinoma@gmail.com>
2022-06-18 21:35:09 +0200

    added exception layer

---
+++ src/Exception/MenuCannotBeSavedException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Evrinoma\MenuBundle\Exception;
+
+class MenuCannotBeSavedException extends \Exception
+{
+
+}
---
+++ src/Exception/MenuInvalidException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Evrinoma\MenuBundle\Exception;
+
+class MenuInvalidException extends \Exception
+{
+
+}
